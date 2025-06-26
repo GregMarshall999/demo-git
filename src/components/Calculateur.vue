9805ceb860c271173d2ce707dad171164b0f8a9d
Greg <gregory.marshall999@gmail.com>
2025-06-26 11:37:07 +0200

    renomage composant0 calculateur.vue
    ajout tout dans repository

---
+++ src/components/Calculateur.vue
@@ -0,0 +1,49 @@
+<template>
+    <Label>A</Label>
+    <input v-model="a"/>
+
+    <Label>B</Label>
+    <input v-model="b"/>
+
+    <label>C</label>
+    <p>{{ c }}</p>
+
+    <label>Opération</label>
+    <select v-model="operator">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+
+    <button @click="calculate">Calculate</button>
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+const a = ref();
+const b = ref();
+const c = ref();
+
+const operator = ref();
+
+const calculate = () => {
+    switch(operator.value) {
+        case "+": 
+            c.value = a.value + b.value;
+            break;
+        case "+": 
+            c.value = a.value - b.value;
+            break;
+        case "*":
+            c.value = a.value * b.value;
+            break;
+        case "/":
+            c.value = a.value / b.value;
+            break;
+    }
+}
+
+</script>
